2f85a694a510882ca6d6436d557a47059ce067ac
ABEL <abelovearn@gmail.com>
2023-04-22 16:47:40 +0300

    add make_cap_first() with docstring in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def make_cap():
     user_line = input(str)
     return user_line.upper()
+
+
+def make_cap_first():
+    """каждое слово строки с залавной буквы"""
+    user_line = input(str)
+    return user_line.title()
